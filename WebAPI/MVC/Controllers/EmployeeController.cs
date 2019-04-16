00c0488b5ee65da4afb3fb9f2a9f31201aaa44ca
ManiSJ <ManiSJ@users.noreply.github.com>
2019-04-16 15:29:57 +1000

    API

---
+++ WebAPI/MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using System.Net.Http;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new mvcEmployeeModel());
+            }
+            else
+            {
+                mvcEmployeeModel emp;
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee/"+id.ToString()).Result;
+                emp = response.Content.ReadAsAsync<mvcEmployeeModel>().Result;
+                return View(emp);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(mvcEmployeeModel emp)
+        {
+            if (emp.EmployeeID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/"+emp.EmployeeID, emp).Result;
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employee/" + id.ToString()).Result;
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebAPI/ReadMe.txt
@@ -0,0 +1,2 @@
+1. MVC Project - CRUD operation sent to WebAPI Project using HttpWebClient
+2. WebAPI Project - Controller with action using Entity Framework
